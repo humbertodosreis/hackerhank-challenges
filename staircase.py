a08f88d4095b7cd790041117e0a77ff443e9b306
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2016-09-23 15:03:34 -0300

    Inclusão de scripts

---
+++ staircase.py
@@ -0,0 +1,7 @@
+﻿import sys
+
+n = int(input().strip())
+
+for i in range(1, n + 1):
+    print(str("#" * i).rjust((n + 1)-1, " "))
+
---
+++ time-conversion.py
@@ -0,0 +1,15 @@
+﻿import sys
+
+time = input().strip()
+hour, min, sec = time[:-2].split(":")
+meridian = time[-2:]
+hour = int(hour)
+
+if meridian == "AM":
+    if hour == 12:
+        hour-= 12
+else:
+    if hour < 12:
+        hour+= 12
+
+print("%s:%s:%s" % (str(hour).rjust(2, "0"), min, sec))
